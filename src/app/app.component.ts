5e18c2ffddfbb3db7513c904b61629ba7ee5a7bf
Charlene Gipulan <charlenegipulan@gmail.com>
2020-09-02 23:07:59 -0700

    use angular jwt to decode token and fix persisting display name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DatingApp-SPA';
+
+export class AppComponent implements OnInit{
+  jwtHelper = new JwtHelperService();
+
+  constructor(private authService: AuthService) { }
+
+  //when the component loads, this is the one we'll use to get token from local storage,
+  //if we have a token, well set the decoded token in the auth service to that token but decoded.
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      //when our application loads, the auth service is gonna have a decoded token as long as
+      //there's a token available in local storage
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
