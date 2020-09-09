b5cbbe3f1a96408453680e63e8f0eb15c1103e5c
Charlene Gipulan <charlenegipulan@gmail.com>
2020-09-08 21:21:28 -0700

    create user.service

--- src/app/_services/dist/auth.service.js
+++ src/app/_services/dist/auth.service.js
@@ -9,19 +9,33 @@ exports.__esModule = true;
 exports.AuthService = void 0;
 var core_1 = require("@angular/core");
 var operators_1 = require("rxjs/operators");
+var angular_jwt_1 = require("@auth0/angular-jwt");
+var environment_1 = require("src/environments/environment");
 var AuthService = /** @class */ (function () {
     function AuthService(http) {
         this.http = http;
-        this.baseUrl = 'http://localhost:5000/api/auth';
+        this.baseUrl = environment_1.environment.apiUrl + 'auth/';
+        this.jwtHelper = new angular_jwt_1.JwtHelperService();
     }
     AuthService.prototype.login = function (model) {
+        var _this = this;
         return this.http.post(this.baseUrl + 'login', model)
             .pipe(operators_1.map(function (response) {
             var user = response;
             if (user) {
                 localStorage.setItem('token', user.token);
+                _this.decodedToken = _this.jwtHelper.decodeToken(user.token);
+                console.log(_this.decodedToken);
             }
         }));
+    };
+    AuthService.prototype.register = function (model) {
+        return this.http.post(this.baseUrl + 'register', model);
+    };
+    AuthService.prototype.loggedIn = function () {
+        var token = localStorage.getItem('token');
+        //if token not expired, return true, if expired return false
+        return !this.jwtHelper.isTokenExpired(token);
     };
     AuthService = __decorate([
         core_1.Injectable({
